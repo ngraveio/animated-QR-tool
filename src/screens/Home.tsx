8a76b45b40afbbafebd86d96bb0295eb92af4b2e
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-03-27 14:47:49 +0400

    gaps between buttons in HomeScreen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,26 @@
 import React, { FC } from "react";
-import { Button } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 import { RootStackScreenProps } from "../navigators/types";
 
 type Props = RootStackScreenProps<"Home">;
 
 const HomeScreen: FC<Props> = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Button title="Scan QR" onPress={() => navigation.navigate("ScanQR")} />
       <Button
         title="Generate QR"
         onPress={() => navigation.navigate("GenerateQR")}
       />
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default HomeScreen;
