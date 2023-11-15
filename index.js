b8f11234c354658d9d37bacc906fd7fbd95e915c
irfan798 <irfanbilaloglu@gmail.com>
2023-11-15 14:23:56 +0300

    chore: make ethers work in hermes, update expo

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+/** To make coin packages work */
+// Import the crypto getRandomValues shim (**BEFORE** the shims)
+import "react-native-get-random-values"
+
+// Import the the ethers shims (**BEFORE** ethers)
+import "@ethersproject/shims"
+
+/** End of: To make coin packages work */
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+
+const a = Buffer.from("hello world");
+console.log(a.toString('utf-8'));
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
