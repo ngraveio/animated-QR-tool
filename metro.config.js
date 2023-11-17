c68938196339b371efa67d3899ef0bf09b065d18
irfan798 <irfanbilaloglu@gmail.com>
2023-11-17 21:18:56 +0300

    chore(metro): use memfs as fs replacement, enable HierarchicalLookup

--- metro.config.js
+++ metro.config.js
@@ -18,11 +18,18 @@ const config = getDefaultConfig(projectRoot);
 config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
+  path.resolve(projectRoot, 'node_modules'),
 ];
+
+// Whether to automatically resolve references to first-party packages (e.g. workspaces) in your project. Any package.json file with a valid name property within projectRoot or watchFolders (but outside of node_modules) counts as a package for this purpose. Defaults to false.
+config.resolver.enableGlobalPackages = true;
+
+// If we enable this, then dependencies cannot import their own dependencies from their path eg: ethereum-cryptography v2.0.0 and v0.0.2
+// root/node_modules/@ethereumjs/util/node_modules/ethereum-cryptography/index
+// root/node_modules/ethereum-cryptography/index
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = false; // true
 
 // 4. Enable symlinks support
 config.resolver.unstable_enableSymlinks = true;
@@ -35,8 +42,10 @@ config.resolver.extraNodeModules = {
   stream: require.resolve("readable-stream"),
   // needed for @keystonehq/ur-decoder
   crypto: require.resolve("react-native-crypto-js"),
+  // needed for ?
+  fs: require.resolve("memfs"),
 };
 
-console.log(config.resolver.extraNodeModules);
+console.log(config.resolver);
 
 module.exports = config;
